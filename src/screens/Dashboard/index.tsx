c381673ffbe85fb1cf3570cd83266a76027e3e25
Eduardo Budny <eduardo.budny@laura-br.com>
2021-06-19 21:19:58 -0300

    created the base for transactions

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard } from '../../components/TransactionCard';
 
 import { 
   Container,
@@ -11,7 +12,9 @@ import {
   UserName,
   UserWrapper,
   Icon,
-  HighlightCards
+  HighlightCards,
+  Transactions,
+  Title,
 } from './styles'
 
 export function Dashboard(){
@@ -49,6 +52,11 @@ export function Dashboard(){
         type="total"
         />
       </HighlightCards>
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionCard />
+      </Transactions>
     </Container>
   )
 }
